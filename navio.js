63824ee0bd38f0acd4e06ee987a2e23bc86115b9
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-06-27 10:25:53 -0300

    Add files via upload

---
+++ navio.js
@@ -0,0 +1,20 @@
+class Barco{
+    constructor(x,y){
+        this.imagem = loadImage("barco.png")
+        this.body = Bodies.circle(x,y,70, {isStatic:false})
+        World.add(world, this.body)
+   
+        this.speed = 0;
+        this.size = 140
+    }
+
+    animar(){
+        this.speed += 0.05;
+    }
+
+    show(){
+        
+        var pos = this.body.position;
+        image (this.imagem, pos.x, pos.y, this.size,this.size)
+    }
+}
